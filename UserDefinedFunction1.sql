c85aca0b44c19ba07106ce8a16f72008cff87c2e
nik7778 <107151173+nik7778@users.noreply.github.com>
2023-07-03 21:46:33 +0300

    Update UserDefinedFunction1.sql

--- UserDefinedFunction1.sql
+++ UserDefinedFunction1.sql
@@ -4,11 +4,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
+
 ALTER FUNCTION [dbo].[GetStringVariableFromTable]
 (	
    @xmlFile xml
