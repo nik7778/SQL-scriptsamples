9416e427d8e8bf162c5a57b43c05a32150eaae7d
nik7778 <107151173+nik7778@users.noreply.github.com>
2023-07-03 21:32:37 +0300

    Update and rename UF2.sql to UserDefinedFunction2.sql

---
+++ UserDefinedFunction2.sql
@@ -0,0 +1,16 @@
+﻿USE [MKI]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER FUNCTION [dbo].[ReadXml] 
+(
+   @xmlFile xml
+)
+RETURNS @Table Table (ID varchar(max)) 
+BEGIN 
+	INSERT INTO @Table 
+	SELECT t.c.value('.', 'VARCHAR(100)') ID FROM @xmlFile.nodes('IdCollection/*') t(c)
+RETURN 
+END 
