26b2cf0b239eed1b9489a0bddd7c13dc8e7ad6e6
nik7778 <107151173+nik7778@users.noreply.github.com>
2023-07-03 21:32:16 +0300

    Update and rename UF1.sql to UserDefinedFunction1.sql

---
+++ UserDefinedFunction1.sql
@@ -0,0 +1,33 @@
+﻿USE [MKI]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER FUNCTION [dbo].[GetStringVariableFromTable]
+(	
+   @xmlFile xml
+)
+RETURNS varchar(max)
+AS
+BEGIN
+
+DECLARE @setOfIds varchar(max);
+
+	SELECT @setOfIds = tb.IdCollection FROM (Select distinct 
+		substring(
+			(
+				Select ',' + cast(ST1.ID as varchar(100))  AS [text()]
+				From (SELECT * FROM [dbo].[ReadXml] (@xmlFile)) ST1
+				For XML PATH ('')
+			), 2, 1000) [IdCollection]
+	From (SELECT * FROM [dbo].[ReadXml] (@xmlFile)) Main) tb
+
+RETURN @setOfIds
+END
+
